f0d5bba5f558a7da92ed7e979427ced49f197593
guifs100 <guifs100@gmail.com>
2024-04-29 22:26:32 -0300

    Adiciona JWT no código, adiciona env no docker compose, configura host e pequenos ajustes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
+
+  def jwt_decodification(token)
+    decoded = JWT.decode(token, ENV['JWT_TOKEN'], true, algorithm: 'HS256')
+  end
 end
