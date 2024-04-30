a6cd03fced49e2ef726f24807937afba2563eaf1
guifs100 <guifs100@gmail.com>
2024-04-29 22:42:58 -0300

    Transforma a decodificação em privada

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
 
+  private
+
   def jwt_decodification(token)
     decoded = JWT.decode(token, ENV['JWT_TOKEN'], true, algorithm: 'HS256')
   end
